d03f554a07cfbf6ab0bdb7e7a6c6df3624ac76da
guillermogonzalez76 <guillermo.gonzalez76@educa.madrid.com>
2026-03-14 18:19:18 +0100

    He cambiado el json, he creado main, y he creado train

---
+++ chatbot_venv/main.py
@@ -0,0 +1,5 @@
+from  flask import Flask, jsonify, request
+
+app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ chatbot_web/model/train.py
@@ -0,0 +1,6 @@
+import random 
+import json 
+import pickle
+import nltk 
+from nltk.stem import WordNetLemamatizer
+import numpy as np
