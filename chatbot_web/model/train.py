cab587b133fcefc53786684ef95030f448cfc098
guillermogonzalez76 <guillermo.gonzalez76@educa.madrid.com>
2026-03-21 10:50:19 +0100

    He creado requereiments.txt para hacer un comando y que se instalen los paquetes necesarios

--- chatbot_web/model/train.py
+++ chatbot_web/model/train.py
@@ -4,3 +4,7 @@ import pickle
 import nltk 
 from nltk.stem import WordNetLemamatizer
 import numpy as np
+
+#Descargo los paquetes que necesitamos de nltk
+nltk.download("punkt")
+nltk.download("wordnet")
